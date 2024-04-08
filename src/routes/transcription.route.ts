fa0024b59f564c072f20fefba656bcf24377347a
Yohannes Kindu Mekonnen <JohnHacker1998>
2024-04-08 19:10:43 +0300

    added transcription feature

--- src/routes/transcription.route.ts
+++ src/routes/transcription.route.ts
@@ -3,18 +3,11 @@ import expressAsyncHandler from 'express-async-handler';
 import { validateSchema } from '../middlewares/validation.middleware';
 import { validateToken } from '../middlewares/validate-token.middleware';
 import { validateRole } from '../guards/role.guard';
-import { GUARD_TYPES } from '../config/other-types/Enums';
-import { join } from 'path';
+import { GUARD_TYPES, LANGUAGES } from '../config/other-types/Enums';
+
+import { get } from '../controllers/transcription.controller';
 
 export const transcriptionRouter: Router = express.Router();
 
-transcriptionRouter.get(
-  '/amharic/:audioName',
-  expressAsyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const audioName: string = req.params.audioName;
-
-      res.sendFile(join(process.cwd(), 'transcriptions/amharic/' + audioName));
-    }
-  )
-);
+transcriptionRouter.get('/amharic/:audioName', get(LANGUAGES.amharic));
+transcriptionRouter.get('/oromiffa/:audioName', get(LANGUAGES.oromiffa));
